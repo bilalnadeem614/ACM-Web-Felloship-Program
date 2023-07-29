4af37c2a42a28c0048e271346518838ae23ea079
Bilal Nadeem <bilalnadeem883@gmail.com>
2023-07-29 12:16:20 +0500

    Adding New Challenges

---
+++ Javascript/challenges_var.js
@@ -0,0 +1,32 @@
+//Challenge-1
+let myBoolean = true;
+
+//Challenge-2
+const myString="hello world";
+
+//Challenge-3
+const firstNumber=20;
+
+//Challenge-4
+let secondNumber=40;
+
+//Challenge-5
+secondNumber=80;
+
+//Challenge-6
+const myArray=[myBoolean,myString];
+
+//Challenge-7
+const myObject={
+    firstProperty:myArray,
+    sumProperty:firstNumber+secondNumber
+}
+
+//Challenge-8
+console.log(myObject.firstProperty)
+
+//Challenge-9
+console.log(myObject.sumProperty)
+
+//Challenge-10
+console.log(myObject.firstProperty[1])
